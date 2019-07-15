8066973a133fb3ec83212a7d5a046f9449b567db
Jonas Hagberg <jonas.hagberg@nbis.se>
2019-07-15 10:28:56 +0200

    Only able to select active rabbits

--- app.R
+++ app.R
@@ -29,9 +29,11 @@ start_time <- Sys.time()
 if (file.exists(Pedifilename)){
   load(file=Pedifilename)
 }else {
-  gottis <-read_excel(paste0('./',filename, '.xlsx'), range = cell_cols("A:L"), col_types = c("skip", "text", "skip", "skip","text", "text", "numeric", "date", "skip", "text","skip", "text"))
-  gottis <- gottis[,c("Nummer","Far nr","Mor nr","Kön","År","Namn","Född")]
-  names(gottis)<-c("Indiv","Sire","Dam","Sex","Born","Name","Född")
+  gottis <-read_excel(paste0('./',filename, '.xlsx'), range = cell_cols("A:V"), col_types = c("skip", "text", "skip", "skip","text", "text", "numeric", "date", "skip", "text","skip", "text","skip","skip","skip","skip","skip","skip","skip","skip","numeric","numeric"))
+
+  
+  gottis <- gottis[,c("Nummer","Far nr","Mor nr","Kön","År","Namn","Född","2019","2018")]
+  names(gottis)<-c("Indiv","Sire","Dam","Sex","Born","Name","Född","Nitton","Arton")
   #Ta bort tomma rader
   gottis<-gottis[!is.na(gottis$Indiv),]
   #rensa bort kastrat
@@ -49,8 +51,8 @@ if (file.exists(Pedifilename)){
       pKin   <- pedIBD(Pedi)
       save(pKin,file=pKinfilename)
     }
-males<-subset(Pedi[with(Pedi,order(Born,decreasing =TRUE)),],Sex=="male")
-females<-subset(Pedi[with(Pedi,order(Born,decreasing =TRUE)),],Sex=="female")
+males<-subset(Pedi[with(Pedi,order(Born,decreasing =TRUE)),],Sex=='male' & (!is.na(Arton) | !is.na(Nitton)))
+females<-subset(Pedi[with(Pedi,order(Born,decreasing =TRUE)),],Sex=="female" & (!is.na(Arton) | !is.na(Nitton)))
 
 
 end_time <- Sys.time()
