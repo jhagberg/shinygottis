0bd20efe1b0756b9743164bb2bfd8364fc2c33fe
Jonas Hagberg <hagberg.jonas@gmail.com>
2020-01-17 20:56:58 +0000

    update to reflect datbase verson as Filename version

--- app.R
+++ app.R
@@ -19,8 +19,8 @@ suppressPackageStartupMessages(is_installed <- require(visPedigree))
 
 
 #Uppdatera namn och datum när en ny lista skapats
-filename<-"Mellerudskaninregister_2018_07-12"
-GDBversion<-"M20190712"
+filename<-"M20191125"
+GDBversion<-filename
 Pedifilename<-paste0('Pedi',filename, '.rdata')
 pKinfilename<-paste0('pKin',filename, '.rdata')
 
